d83717f6e1309ebb9fadf29617442dc82ed5c86a
Sabrina Jiang <sabrinajiang@Sabrinas-MacBook-Pro.local>
2017-06-12 10:10:15 -0700

    formatted code

--- demoapp-ios-swift/ViewController.swift
+++ demoapp-ios-swift/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var controlLabel: UILabel!
     @IBOutlet weak var crash1Button: UIButton!
     @IBOutlet weak var crash2Button: UIButton!
@@ -20,38 +20,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func leftButtonClick(_ sender: UIButton) {
+
+    @IBAction func leftButtonClick(_: UIButton) {
         // display the following text within the label when the left button is pressed.
         controlLabel.text = "To the left."
     }
-    
-    @IBAction func rightButtonClick(_ sender: UIButton) {
+
+    @IBAction func rightButtonClick(_: UIButton) {
         // display the following text within the label when right button is pressed.
         controlLabel.text = "In the right."
     }
-    
+
     @IBAction func activateCrash(_ sender: UIButton) {
         switch sender {
         case crash1Button:
-            print ("crash 1 activated, should be Fatal Error")
+            print("crash 1 activated, should be Fatal Error")
             fatalError()
-            
+
         case crash2Button:
-            print ("crash 2 activated, should be found nil in optional value")
+            print("crash 2 activated, should be found nil in optional value")
             let n: Int? = nil
             let val = n!
-            
+
         default:
             break
-
         }
-
     }
-
 }
