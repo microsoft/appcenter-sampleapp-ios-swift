451afd29edb4129d11592d291b4f63ff994b6b19
Tina <tina@Tinas-MBP.guest.corp.microsoft.com>
2017-07-05 15:04:32 -0700

    pageViewController with 6 viewControllers

--- demoapp-ios-swift/RootViewController.swift
+++ demoapp-ios-swift/RootViewController.swift
@@ -3,27 +3,44 @@
   
 class RootViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
     
-    let pages = ["crashViewController", "analyticsViewController"]
+    // give a list of service
+    var services: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Here, we manually add our services to the Data Model
+        services = ["welcomeViewController",
+                    "buildViewController",
+                    "testViewController",
+                    "distributeViewController",
+                    "crashViewController",
+                    "analyticsViewController",
+                    "pushViewController"]
+        
         self.delegate = self
         self.dataSource = self
-            
-        let vc = self.storyboard?.instantiateViewController(withIdentifier: "crashViewController")
-        setViewControllers([vc!], // Has to be a single item array, unless you're doing double sided stuff I believe
+        
+        // set the starting viewController, in this case it is welcomeViewController
+        let startingViewController = self.storyboard?.instantiateViewController(withIdentifier: "welcomeViewController")
+        setViewControllers([startingViewController!],
             direction: .forward,
             animated: true,
             completion: nil)
     }
-
     
-    func pageViewController(_ pageViewController: UIPageViewController,
-                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+    override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+           }
+
+    //MARK: - UIPageViewController protocol functions
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         if let identifier = viewController.restorationIdentifier {
-            if let index = pages.index(of: identifier) {
+            if let index = services.index(of: identifier) {
                 if index > 0 {
-                    return self.storyboard?.instantiateViewController(withIdentifier: pages[index-1])
+                    return self.storyboard?.instantiateViewController(withIdentifier: services[index-1])
                 }
             }
         }
@@ -33,9 +50,9 @@ class RootViewController: UIPageViewController, UIPageViewControllerDelegate, UI
     func pageViewController(_ pageViewController: UIPageViewController,
                             viewControllerAfter viewController: UIViewController) -> UIViewController? {
         if let identifier = viewController.restorationIdentifier {
-            if let index = pages.index(of: identifier) {
-                if index < pages.count - 1 {
-                    return self.storyboard?.instantiateViewController(withIdentifier: pages[index+1])
+            if let index = services.index(of: identifier) {
+                if index < services.count - 1 {
+                    return self.storyboard?.instantiateViewController(withIdentifier: services[index+1])
                 }
             }
         }
@@ -43,15 +60,10 @@ class RootViewController: UIPageViewController, UIPageViewControllerDelegate, UI
     }
     
     func presentationCount(for pageViewController: UIPageViewController) -> Int {
-        return pages.count
+        return services.count
     }
     
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-        if let identifier = viewControllers?.first?.restorationIdentifier {
-            if let index = pages.index(of: identifier) {
-                return index
-            }
-        }
         return 0
     }
 }
