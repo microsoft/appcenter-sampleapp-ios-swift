200d8795419209471507d9ca3cf8d046a2629686
Tina <tina@Tinas-MBP.guest.corp.microsoft.com>
2017-07-05 15:59:02 -0700

    fully functional pageViewController app

--- demoapp-ios-swift/RootViewController.swift
+++ demoapp-ios-swift/RootViewController.swift
@@ -33,30 +33,41 @@ class RootViewController: UIPageViewController, UIPageViewControllerDelegate, UI
             super.didReceiveMemoryWarning()
             // Dispose of any resources that can be recreated.
            }
+    
+    // MARK: - UIPageViewController navigation functions
+    // taken from https://stackoverflow.com/questions/17779735/how-do-i-make-the-bottom-bar-with-dots-of-a-uipageviewcontroller-translucent
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if let scrollView = view.subviews.filter({ $0 is UIScrollView }).first,
+            let pageControl = view.subviews.filter({ $0 is UIPageControl }).first {
+            scrollView.frame = view.bounds
+            view.bringSubview(toFront:pageControl)
+        }
+    }
 
     //MARK: - UIPageViewController protocol functions
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        if let identifier = viewController.restorationIdentifier {
-            if let index = services.index(of: identifier) {
-                if index > 0 {
-                    return self.storyboard?.instantiateViewController(withIdentifier: services[index-1])
-                }
-            }
+        let index = services.index(of: viewController.restorationIdentifier!)
+        if (index == 0) || (index == NSNotFound) {
+            return nil
         }
-        return nil
+            
+        else {
+            return self.storyboard?.instantiateViewController(withIdentifier: services[index!-1])
+        }
     }
     
     func pageViewController(_ pageViewController: UIPageViewController,
                             viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        if let identifier = viewController.restorationIdentifier {
-            if let index = services.index(of: identifier) {
-                if index < services.count - 1 {
-                    return self.storyboard?.instantiateViewController(withIdentifier: services[index+1])
-                }
-            }
+        let index = services.index(of: viewController.restorationIdentifier!)
+        if index! < services.count - 1 {
+            return self.storyboard?.instantiateViewController(withIdentifier: services[index!+1])
         }
-        return nil
+        else {
+            return nil
+        }
     }
     
     func presentationCount(for pageViewController: UIPageViewController) -> Int {
