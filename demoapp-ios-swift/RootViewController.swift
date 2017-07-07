34de8d67668acacc0fafc8b1ce65abcebd8249c2
Tina <tina@Tinas-MBP.guest.corp.microsoft.com>
2017-07-06 17:32:05 -0700

    Demo App iOS v1

--- demoapp-ios-swift/RootViewController.swift
+++ demoapp-ios-swift/RootViewController.swift
@@ -1,81 +1,80 @@
 
-  import UIKit
-  
+import UIKit
+
 class RootViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
-    
+
     // give a list of service
     var services: [String] = []
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Here, we manually add our services to the Data Model
-        services = ["welcomeViewController",
-                    "buildViewController",
-                    "testViewController",
-                    "distributeViewController",
-                    "crashViewController",
-                    "analyticsViewController",
-                    "pushViewController"]
-        
-        self.delegate = self
-        self.dataSource = self
-        
+        services = [
+            "welcomeViewController",
+            "buildViewController",
+            "testViewController",
+            "distributeViewController",
+            "crashViewController",
+            "analyticsViewController",
+            "pushViewController",
+        ]
+
+        delegate = self
+        dataSource = self
+
         // set the starting viewController, in this case it is welcomeViewController
         // TODO: change to tableViewController
-        let startingViewController = self.storyboard?.instantiateViewController(withIdentifier: "welcomeViewController")
+        let startingViewController = storyboard?.instantiateViewController(withIdentifier: "welcomeViewController")
         setViewControllers([startingViewController!],
-            direction: .forward,
-            animated: true,
-            completion: nil)
+                           direction: .forward,
+                           animated: true,
+                           completion: nil)
     }
-    
+
     override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
-           }
-    
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
     // MARK: - UIPageViewController navigation functions
     // taken from https://stackoverflow.com/questions/17779735/how-do-i-make-the-bottom-bar-with-dots-of-a-uipageviewcontroller-translucent
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+
         if let scrollView = view.subviews.filter({ $0 is UIScrollView }).first,
             let pageControl = view.subviews.filter({ $0 is UIPageControl }).first {
             scrollView.frame = view.bounds
-            view.bringSubview(toFront:pageControl)
+            view.bringSubview(toFront: pageControl)
         }
     }
 
-    //MARK: - UIPageViewController protocol functions
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+    // MARK: - UIPageViewController protocol functions
+
+    func pageViewController(_: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         let index = services.index(of: viewController.restorationIdentifier!)
         if (index == 0) || (index == NSNotFound) {
             return nil
-        }
-            
-        else {
-            return self.storyboard?.instantiateViewController(withIdentifier: services[index!-1])
+        } else {
+            return storyboard?.instantiateViewController(withIdentifier: services[index! - 1])
         }
     }
-    
-    func pageViewController(_ pageViewController: UIPageViewController,
+
+    func pageViewController(_: UIPageViewController,
                             viewControllerAfter viewController: UIViewController) -> UIViewController? {
         let index = services.index(of: viewController.restorationIdentifier!)
         if index! < services.count - 1 {
-            return self.storyboard?.instantiateViewController(withIdentifier: services[index!+1])
-        }
-        else {
+            return storyboard?.instantiateViewController(withIdentifier: services[index! + 1])
+        } else {
             return nil
         }
     }
-    
-    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+
+    func presentationCount(for _: UIPageViewController) -> Int {
         return services.count
     }
-    
-    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+
+    func presentationIndex(for _: UIPageViewController) -> Int {
         return 0
     }
 }
