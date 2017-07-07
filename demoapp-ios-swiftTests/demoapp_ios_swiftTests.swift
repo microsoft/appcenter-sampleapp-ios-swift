8401f33097b1324dff3d0e39db9079d8c444e083
Sherin Shibu <sherin@Sherins-MacBook-Pro.local>
2017-07-06 18:24:01 -0700

    fixed test

--- demoapp-ios-swiftTests/demoapp_ios_swiftTests.swift
+++ demoapp-ios-swiftTests/demoapp_ios_swiftTests.swift
@@ -27,9 +27,9 @@ class demoapp_ios_swiftTests: XCTestCase {
         // make sure that when the buttons are tapped, the correct text pops up.
         let model = XCUIApplication()
         model.buttons["customEventButton"].tap()
-       let labelValue = model.staticTexts["controlLabel"]
+       let labelValue = model.textFields["controlLabel"]
        // XCTAssert(true) is the basis of the test
-        XCTAssert(labelValue.label == "sample event button pressed")
+        XCTAssert(labelValue.title == "sample event button pressed")
     }
 
     // assert that pageview is on the screen
